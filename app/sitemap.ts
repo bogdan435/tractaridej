118fb2ce0b5a782b7da540a6358e5e3ef66c4d3c
Bogdan <bogdan@alphacont.ro>
2026-05-21 13:45:47 +0300

    Use non-www sitemap URL

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.tractaridej.com',
+      url: 'https://tractaridej.com',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
